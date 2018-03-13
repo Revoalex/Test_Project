bff35c8ed9a10b4c46fc9f46f73b4284b6d4bd9d
Revoalex <Mystatoprya@gmail.com>
2018-03-13 11:21:34 +0200

    Test

--- WFA-Test/Class1.cs
+++ WFA-Test/Class1.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
 
 namespace WFA_Test
 {
     class Class1
     {
+       public void test()
+        {
+            MessageBox.Show("asda");
+        }
     }
 }
